443803735c5d9da2e8e158ce680baeeba3394035
noemi3a <nhenrique@3anet.com.br>
2024-03-04 15:08:00 -0300

    Antes de terminar barra de pesquisa

--- script.js
+++ script.js
@@ -23,3 +23,26 @@ const containerVideos = document.querySelector(".videos__container");
 
 
   
+//Código omitido
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa(){
+    const videos = document.querySelectorAll(".videos__item");
+
+    if(barraDePesquisa.value != ""){
+        for(let video of videos){
+            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+            if(!titulo.includes(valorFiltro)){
+                video.style.display = "none";
+            } else {
+                video.style.display = "block";
+            }
+
+        }
+      }
+    }
